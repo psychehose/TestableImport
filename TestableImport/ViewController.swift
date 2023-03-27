56c79bde2ca9a555f39d47ea0c8fc5227da28481
psychehose <psychehose@gmail.com>
2023-03-28 01:19:13 +0900

    프로젝트 생성

---
+++ TestableImport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestableImport
+//
+//  Created by 김호세 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
