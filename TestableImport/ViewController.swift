21f9fafe3a40e1b2cdbeea6ea19470f3b82d4a48
psychehose <psychehose@gmail.com>
2023-03-28 01:56:35 +0900

    버그 재현

--- TestableImport/ViewController.swift
+++ TestableImport/ViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+  private let text: String
+
+  init(text: String) {
+    self.text = text
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    view.backgroundColor = .red
+    guard let bundleID = Bundle.main.bundleIdentifier else {
+      return
+    }
+
+
+
+    debugPrint("Bundle ID ==== ", bundleID)
   }
 
 
